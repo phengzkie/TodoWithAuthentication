ea9f1dfdec13adf14ae0123eb11e08a0917d6524
phengzkie <alfie.osayan@gmail.com>
2019-08-02 15:03:24 +0800

    update env config

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,7 +4,7 @@ import { FETCH_USER } from './types';
 const fetchUser = () => {
     return function(dispatch) {
         axios.get('/api/current_user')
-            .then(res => dispatch({ type: FETCH_USER, payload: res }))
+            .then(res => dispatch({ type: FETCH_USER, payload: res }));
     }
 };
 
